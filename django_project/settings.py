e04bd2ad15210f7076460a393b6a8f7e9617848b
Ahmad Muhammad <info@ahmadmuhammad.co.za>
2020-12-20 11:57:47 +0200

    added config.json

--- django_project/settings.py
+++ django_project/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import json
+
+with open(os.path.abspath('django_project/config.json')) as config_file:
+    config = json.load(config_file)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('CHIRPY_SECRET_KEY')
+SECRET_KEY = config['CHIRPY_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
